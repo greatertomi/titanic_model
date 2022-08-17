6cfa54bfba70fc0a5390c317e05ef7d465deca44
John Oshalusi <john.oshalusi@iohk.io>
2022-08-17 17:39:16 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Titanic AI Model Package
+
+This package uses [tox](https://tox.wiki/en/latest/) for automation
+
+## Commands
+
+- `tox -e test_package` to run tests
+- `tox -e train` to train model
+- `tox -e lint` run linter through the code
---
+++ tests/test_features.py
@@ -0,0 +1,12 @@
+from regression_model.config.core import config
+from regression_model.processing.features import ExtractLetterTransformer
+
+
+def test_extract_letter_transformer(sample_input_data):
+    transformer = ExtractLetterTransformer(variables=config.model_config.cabin)
+    assert sample_input_data['cabin'].iat[0] == 'B5'
+
+    subject = transformer.fit_transform(sample_input_data)
+
+    assert subject['cabin'].iat[0] == 'B'
+
