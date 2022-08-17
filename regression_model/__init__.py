074380aa61e3ad93cefc3c7753b1c3481107da31
John Oshalusi <john.oshalusi@iohk.io>
2022-08-17 16:02:08 +0100

    Create config files

---
+++ regression_model/__init__.py
@@ -0,0 +1,3 @@
+import logging
+
+from regression_model.config.core import PACKAGE_ROOT
---
+++ regression_model/processing/validation.py
@@ -0,0 +1,9 @@
+import typing as t
+from pathlib import Path
+
+import joblib
+import pandas as pd
+from sklearn.pipeline import Pipeline
+
+from regression_model import __version__ as _version
+from regression_model.config.core import DATASET_DIR, TRAINED_MODEL_DIR, config
